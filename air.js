dc6f9c5558e3f0aa447dae09aaffb87631cff324
Erickson Santos <121777387+Erickbeast1@users.noreply.github.com>
2023-07-01 11:14:24 -0400

    first commit

---
+++ air.js
@@ -0,0 +1,10 @@
+let baseUrl = 'wp3360984.web'
+let new1 = 'wp3360984.webp',baseUrl
+document.getElementById('pic01').onclick =  tryitout
+function tryitout(){
+    document.querySelector('mainPic').style.backgroundImage ='pic01'
+}
+function baseUrl(){
+    let baseUrl =  new1
+    document.getElementById('mainPic').style.backgroundImage= new1
+}
